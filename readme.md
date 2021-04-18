e8b1cd983169aa403804c4770111c0ac0e24eeec
Roboramv2 <roboramv2@gmail.com>
2021-04-18 12:53:23 +0530

    ultrasonic

--- readme.md
+++ readme.md
@@ -10,3 +10,7 @@ For each component: code, picture, and output are given.
 1. KY-015: (folder named dht)
     * Humidity and Temperature Sensor
     * [Datasheet](https://datasheetspdf.com/pdf-file/1401995/Joy-IT/KY-015/1)
+
+2. HC-SR04: (folder named ultrasonic)
+    * Measure distance using reflection of sound
+    * [Datasheet](https://datasheetspdf.com/pdf-file/1380136/ETC/HC-SR04/1)
---
+++ ultrasonic/ultrasonic.ino
@@ -0,0 +1,26 @@
+#define echoPin 4 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+//Vcc to +5V, Gnd to Ground
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  Serial.println("with Arduino UNO R3");
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance is: ");
+  Serial.println(distance);
+  delay(1000);
+}
